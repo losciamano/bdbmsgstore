6e9a2f2bc12b52586e94d42707f81cae3b403e6a
Dario Mazza <dariomzz@gmail.com>
2012-02-13 21:12:00 +0100

    added gpl3 header and copying

--- lib/JournalImpl.h
+++ lib/JournalImpl.h
@@ -1,25 +1,23 @@
-/*
-  Copyright (c) 2007, 2008, 2009 Red Hat, Inc.
+/**********************************************************************************
+*
+*   bdbmsgstore: BDB-based Message Store Plugin for Apache Qpid C++ Broker
+*   Copyright (C) 2011 Dario Mazza (dariomzz@gmail.com)
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*
+*********************************************************************************/
 
-  This file is part of the Qpid async store library msgstore.so.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
-  USA
-
-  The GNU Lesser General Public License is available in the file COPYING.
-*/
 
 #ifndef _JournalImpl_
 #define _JournalImpl_
--- lib/PidTracker.cpp
+++ lib/PidTracker.cpp
@@ -1,3 +1,24 @@
+/**********************************************************************************
+*
+*   bdbmsgstore: BDB-based Message Store Plugin for Apache Qpid C++ Broker
+*   Copyright (C) 2011 Dario Mazza (dariomzz@gmail.com)
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*
+*********************************************************************************/
+
+
 #include "PidTracker.h"
 #include <iostream>
 
